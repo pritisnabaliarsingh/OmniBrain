79dfbcd582d6db6622db1acf6d8029153a9bfa27
Surya Teja <suryagoshika745>
2026-08-14 12:43:17 +0530

    Integrate Gemini LLM with RAG backend

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,7 +1,13 @@
-import os 
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok = True)
-# Validation constants
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+UPLOAD_DIR = "uploads"
 ALLOWED_TYPES = ["application/pdf"]
-MAX_FILE_SIZE = 10 * 1024 * 1024  # 10 MB                                                                                                                                                                                         
+MAX_FILE_SIZE = 10 * 1024 * 1024
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,17 +1,15 @@
 from fastapi import HTTPException
 
-from app.utils.logger import logger
-
+from app.services.llm_service import generate_answer
 from app.services.embedding_service import create_embeddings
 from app.services.vector_store import search
+from app.utils.logger import logger
 
 
 def ask_rag(request):
-
     try:
-
+        # Validate question
         if not request.question.strip():
-
             logger.warning("Empty question received.")
 
             raise HTTPException(
@@ -21,11 +19,20 @@ def ask_rag(request):
 
         logger.info(f"Question received: {request.question}")
 
+        # Create embedding for the user's question
         query_embedding = create_embeddings([request.question])
 
+        # Search similar chunks from FAISS
         results = search(query_embedding[0])
 
-        answer = results[0]
+        # Convert retrieved chunks into context
+        context = "\n".join(results)
+
+        # Generate answer using Gemini
+        answer = generate_answer(
+            request.question,
+            context
+        )
 
         return {
             "question": request.question,
@@ -36,7 +43,6 @@ def ask_rag(request):
         raise
 
     except Exception as e:
-
         logger.error(f"Unexpected Error: {str(e)}")
 
         raise HTTPException(
