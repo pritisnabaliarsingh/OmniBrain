6c961f70cf6c24cec08dc02c4b523a08d1e71594
Surya Teja <suryagoshika745>
2026-08-16 18:05:25 +0530

    Day 14: organize backend routers

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from app.routers import upload
 from app.routers import ask
-import app.routers.health as health_router
+from app.routers import health
 
 from app.utils.logger import logger
 
@@ -19,6 +19,6 @@ async def startup_event():
 
 app.include_router(upload.router)
 app.include_router(ask.router)
-app.include_router(health_router.router)
+app.include_router(health.router)
     
  
